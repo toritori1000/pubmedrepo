c024e3c68c765a45fddc8f737132e8724cd9d860
victoria <toritori1000@gmail.com>
2019-01-14 16:37:30 -0500

    secret_number code

--- pubmedrepo/src/pubmed/package1/example_mod2.py
+++ pubmedrepo/src/pubmed/package1/example_mod2.py
@@ -5,3 +5,24 @@ class ExampleMod2:
 
     def compose(self):
         return "{} catches {}.".format(self.b, self.a)
+
+
+def new_func(self, a: str, b: int, c: int = 10) -> str:
+    """[summary]
+
+    [description]
+
+    Args:
+        a (str): [description]
+        b (int): [description]
+        c (int, optional): Defaults to 10. [description]
+
+    Returns:
+        str: [description]
+    """
+
+    # regular comment
+    # * important comment
+    # ! warning
+    # TODO: todo stuff
+    # ? question???
